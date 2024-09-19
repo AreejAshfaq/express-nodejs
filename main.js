8de031bfcc88af7dfe3650dab33ce6554d4dc03f
AreejAshfaq <areejch15222@gmail.com>
2024-09-19 08:01:36 -0700

    practis

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 
 
 const http = require("http");
-const fs = require("fs");
-const url =  require("url");
-const express = require("express");
-const app = require("app");
 
-const server = http.createServer(
-);
-app.get("/",(res,req) => {
-     res.send("hellow i an areej")
-    });
-    app.get("about/",(res,req) => {
-     res.send("zimal is here"+" " + req.query.name +" "+ req.query.id)
-    });
+const fs = require("fs");
+
+const url = require("url");
+
+const express = require("express");
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+
+res.send("Hello, I am Areej");
+
+});
+
+app.get("/about", (req, res) => {
+
+res.send("Zimal is here" + " " + req.query.name + " " + req.query.id); });
+
+
      
 server.listen(2224,() => {
     console.log("server is listening too......")
